8111368e7ddc97d4bfc47cff794db74eb3bab31b
FeelPatel <final@aether.co.in>
2026-02-02 19:18:35 +0530

    desci

--- public/Rabbitmq/part4-Topic/producer.js
+++ public/Rabbitmq/part4-Topic/producer.js
@@ -21,6 +21,32 @@
  Delhi Drivers               All Ride Analytics
 
 
+Is Video me ham cover karne wale hai TOPIC EXCHANGE, topic exchnage kse work krta hai, Topic exhange me routing pattern ke basis pe hoti hai
+ye direct exchange se powerfull hota hai kyuki topic exhnage me hum WILDCARD use kar sakte hain.
+TOpic exchange me hum 2 type ke WILDCARD RULES use kar skate hai, * star and 🔥 # (Hash).
+* start bolta hai Exactly ek (ONE) word match karo
+exmaple ke liye samjo hamne Pattern rakhi ride.delhi.*  to ride.delhi. fixed rahega lekin * ki cab,auto,bike koi ek word chalega
+lekin agar ride.delhi.cab.xyz then not match kyuki cab ke bad ek aur extra word hai
+
+# hash bolta hai — Zero OR MORE words, matlab 0, 1, ya unlimited words
+agr hamari pattern ride.# hai, to  message ride se start hona chahiye uske baad kuch bhi aa sakta hai,
+lekin agr myrides.delhi.cab ride me aapne s laga diya to nahi match hoga kyoki hamen bola hai first word ride useke bad kuch bhi
+
+Agar aapko strict control chahiye → * use karo aur Agar aapko flexibility chahiye → # use karo
+
+Topic exchange me routing key and binding key DOT(.) se seperate hona bahut jaruri hai
+aesa manlo DOT compulsory hai, dot ke bina wo pattern samajga hi nahi.
+agar app routing key defined karte ho ride*delhi ride#cab *delhi* ye sub invalid mani jayega topic exhnage me
+
+Topic exchange kab use hota hai
+jab aapko category-wise ya region-wise filtering chahiye.
+
+
+
+
+aur Direct exchange se TOPic exhange powerfull hota hai kyoki isme hum whild card use kar skate hai
+⭐ WILDCARD RULES (VERY IMPORTANT)
+
  🔑 Sabse Important Rule (Yaad Rakhna)
 
 Topic exchange me routing key & binding key
